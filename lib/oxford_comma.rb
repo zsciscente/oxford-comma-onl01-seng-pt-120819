3d84fec91086dffc95052c18d704bc9705e25e9e
Zachary Sciscente <zsciscente@gmail.com>
2019-12-20 03:36:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size == 3
+    array.join(", ") 
+    array.push[-1] = ("and ")
+  end
 end
