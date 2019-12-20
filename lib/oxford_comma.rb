36cbf356774d230bb78b0a35b2b6fa5bbeb8e80a
Zachary Sciscente <zsciscente@gmail.com>
2019-12-20 03:46:27 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array.join(" and ")
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
+    return array.join(", ")
+    
+  end
 end
